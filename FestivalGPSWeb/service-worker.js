7afe379184dc363b970eb4f8c203128efc3c5422
Yangyang Peng <yangyang.peng@oracle.com>
2026-05-13 12:51:37 -0700

    Simplify app icon

--- FestivalGPSWeb/service-worker.js
+++ FestivalGPSWeb/service-worker.js
@@ -1,17 +1,17 @@
-const CACHE_NAME = "festival-gps-v45";
+const CACHE_NAME = "festival-gps-v46";
 const APP_SHELL = [
   "./",
   "./index.html",
-  "./styles.css?v=20260513-10",
-  "./app.js?v=20260513-10",
+  "./styles.css?v=20260513-11",
+  "./app.js?v=20260513-11",
   "./base44-config.js",
   "./firebase-config.js",
-  "./manifest.webmanifest?v=20260513-10",
+  "./manifest.webmanifest?v=20260513-11",
   "./assets/edc-grid-map.jpg?v=20260513-1",
-  "./assets/icon.svg?v=20260513-10",
-  "./assets/apple-touch-icon.png?v=20260513-10",
-  "./assets/icon-192.png?v=20260513-10",
-  "./assets/icon-512.png?v=20260513-10"
+  "./assets/icon.svg?v=20260513-11",
+  "./assets/apple-touch-icon.png?v=20260513-11",
+  "./assets/icon-192.png?v=20260513-11",
+  "./assets/icon-512.png?v=20260513-11"
 ];
 
 self.addEventListener("install", (event) => {
