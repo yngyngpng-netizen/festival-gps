791ee1bdd6f1e63511f2a59ba0a5509a0eda545a
Yangyang Peng <yangyang.peng@oracle.com>
2026-05-13 13:07:37 -0700

    Update onboarding headline

--- FestivalGPSWeb/service-worker.js
+++ FestivalGPSWeb/service-worker.js
@@ -1,17 +1,17 @@
-const CACHE_NAME = "festival-gps-v49";
+const CACHE_NAME = "festival-gps-v50";
 const APP_SHELL = [
   "./",
   "./index.html",
-  "./styles.css?v=20260513-14",
-  "./app.js?v=20260513-14",
+  "./styles.css?v=20260513-15",
+  "./app.js?v=20260513-15",
   "./base44-config.js",
   "./firebase-config.js",
-  "./manifest.webmanifest?v=20260513-14",
+  "./manifest.webmanifest?v=20260513-15",
   "./assets/edc-grid-map.jpg?v=20260513-1",
-  "./assets/icon.svg?v=20260513-14",
-  "./assets/apple-touch-icon.png?v=20260513-14",
-  "./assets/icon-192.png?v=20260513-14",
-  "./assets/icon-512.png?v=20260513-14"
+  "./assets/icon.svg?v=20260513-15",
+  "./assets/apple-touch-icon.png?v=20260513-15",
+  "./assets/icon-192.png?v=20260513-15",
+  "./assets/icon-512.png?v=20260513-15"
 ];
 
 self.addEventListener("install", (event) => {
